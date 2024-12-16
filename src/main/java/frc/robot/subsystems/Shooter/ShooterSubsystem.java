6ee170269045ddb867327a80574bdb1c85b9f61c
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2024-12-15 19:56:59 -0600

    chore: comments

--- src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -8,21 +8,32 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkMax;
 
+// Create the shooter subsystem
 public class ShooterSubsystem extends SubsystemBase {
+    // Define the shooterMotor from CANSparkMax
     private final CANSparkMax shooterMotor;
+
+    // Start to define the motor settings
     public ShooterSubsystem() {
+        // Set it to CAN ID 16
         shooterMotor = new CANSparkMax(16, MotorType.kBrushless);
+        // Restore defaults
         shooterMotor.restoreFactoryDefaults();
+        // Set the motor to break
         shooterMotor.setIdleMode(IdleMode.kBrake);
     }
 
+    // Create the runShooter Command
+    // Take the double speed as the input
+    // Set the motor to speed
     public Command runShooter(double speed){
         return run(() ->
             shooterMotor.set(speed)
         );
     }
 
-    // Auto
+    // Create the stopShooter Command
+    // Sets the shooter motor to 0
     public Command stopShooter(){
         return this.run(() -> 
             shooterMotor.set(0)
